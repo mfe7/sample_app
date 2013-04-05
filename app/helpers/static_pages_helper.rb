6f27d445f16ed8bc5aa575521aedb6255f6887af
Michael <mfe@mit.edu>
2013-04-05 02:26:04 -0400

    Buy tickets link on homepage new direction. Who's going.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,4 +3,12 @@ module StaticPagesHelper
 	def ticket_goal
 		ticket_goal = 100
 	end
+
+	def signed_in?
+  		!current_user.nil?
+  	end
+
+  	def current_user
+  		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  	end
 end
