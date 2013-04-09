0619fcc39f2e398a9fd21edb165b307cffdf0b28
Michael <mfe@mit.edu>
2013-04-08 20:20:11 -0400

    fixed db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130405153001) do
+ActiveRecord::Schema.define(:version => 20130408225306) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(:version => 20130405153001) do
     t.string   "remember_token"
     t.boolean  "admin",             :default => false
     t.integer  "tickets_purchased", :default => 0
-    t.string   "school"
     t.integer  "tickets_wanted",    :default => 0
+    t.string   "school"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
